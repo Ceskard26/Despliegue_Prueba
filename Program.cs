053a56e557ea6150f93a21a5fa3953717e24d25f
Ceskad26 <cesaracarrascor1026@gmail.com>
2025-03-02 19:21:50 -0500

    Corrección de Secrets y configuración de S3

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configurar el pipeline de solicitudes HTTP
+// Configurar el pipeline de solicitudes HTTP.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
